70a3b1b17d0af6e4f774155af283e96ad053eb20
Chris Nunez <cenunez47@gmail.com>
2017-09-17 23:46:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 def speak_to_grandma(say)
   if say == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif say != say.upcase
+    return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
   end
