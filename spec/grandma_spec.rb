42f679f410a29445b6d9647505720e5474a09bc4
Chris Nunez <cenunez47@gmail.com>
2017-09-17 23:41:29 +0000

    automatically backed up by learn

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -11,8 +11,4 @@ describe '#speak_to_grandma' do
     expect(speak_to_grandma('WHAT DID YOU EAT TODAY?')).to eq "NO, NOT SINCE 1938!"
   end
 
-  it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
-    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
-  end
-
 end
